a586460f13d99a2339376e8678cc4e07f9ce7d7d
kip2 <tqa1no4@gmail.com>
2023-10-28 17:07:44 +0900

    classの名前を変更

--- app/src/components/Input.js
+++ app/src/components/Input.js
@@ -4,7 +4,7 @@ import styles from "../css/Input.css";
 export default function Input() {
     return (
         <>
-            <form action="" className={styles.outer}>
+            <form action="" className={styles.container}>
                 <input type="number" min="00" max="24" placeholder="時間"/>
                 <input type="number" min="00" max="60" placeholder="分"/>
                 <input type="number" min="00" max="60"  placeholder="秒"/>
