538740da7c1cab96304e91deb032e04aca4ee658
kip2 <tqa1no4@gmail.com>
2023-10-28 23:51:11 +0900

    add: リセット機能＆一時停止機能

--- app/src/components/CountDownTimer.js
+++ app/src/components/CountDownTimer.js
@@ -13,15 +13,42 @@ export default function CountDownTimer() {
     const [intervalId, setIntervalId] = useState(null);
     const [isActive, setIsActive] = useState(false);
 
+    useEffect(() => {
+        if(isActive && !intervalId) {
+            const id = setInterval(() => {
+                let time = transferSeconds(hour, minutes, second);
+                time = time > 0 ? time - 1 : time;
+                [hour, minutes, second] = formatTime(time);
+                setHour(hour);
+                setMinutes(minutes);
+                setSecond(second);
+
+                if (time <= 0) {
+                    clearInterval(id);
+                    alert("タイマーアップ！");
+                    setIsActive(false);
+                    setIntervalId(null);
+                }
+            }, 1000);
+            setIntervalId(id);
+        }
+
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [isActive, intervalId]);
+
 	const onStartButton = (hour, minutes, second) => {
         // 空白文字なら0に初期化する
         if (hour === "") hour = 0;
         if (minutes === "") minutes = 0;
         if (second === "") second = 0;
 
-        setIsActive(true);
+        // setIsActive(true);
 
-        const intervalTime = setInterval(() => {
+        const id = setInterval(() => {
             let time = transferSeconds(hour, minutes, second);
             time = time > 0 ? time - 1 : time;
             [hour, minutes, second] = formatTime(time);
@@ -30,15 +57,13 @@ export default function CountDownTimer() {
             setSecond(second);
 
             if (time <= 0) {
+                clearInterval(id);
                 alert("タイマーアップ！");
-                clearInterval(intervalTime);
             }
         }, 1000);
-
-        setIntervalId(intervalTime);
+        setIntervalId(id);
 	};
     
-
     useEffect(() => {
         return () => {
             if (intervalId) {
@@ -47,13 +72,29 @@ export default function CountDownTimer() {
         };
     }, [intervalId]);
 
+
     const handlePause = () => {
         setIsActive(false);
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
     };
 
     const handleResume = () => {
         setIsActive(true);
+        if (intervalId) {
+            setIntervalId(intervalId);
+        }
     };
+
+    const handleReset = () => {
+        setHour("");
+        setMinutes("");
+        setSecond("");
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+    }
 
     return (
         <div className={styles.outer}>
@@ -77,6 +118,7 @@ export default function CountDownTimer() {
                 <TimerButtons 
                     onStop={handlePause}
                     onResume={handleResume}
+                    onReset={handleReset}
                 />
             </section>
         </div>
