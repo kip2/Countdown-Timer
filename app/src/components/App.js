db5a2a33b7a9c7c03079eaa81ab8473829e4930c
kip2 <tqa1no4@gmail.com>
2023-10-28 19:00:22 +0900

    責務をAppからCountDownTimerに委譲

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -4,14 +4,10 @@ import CountDownTimer from "./CountDownTimer";
 
 export default function App() {
 
-	const onStartButton = (hour, minutes, second) => {
-		alert(`${hour}:${minutes}:${second}`);
-	};
-
 	return (
 		<>
 			<h1>Count Down Timer</h1>
-			<CountDownTimer onStartButton={onStartButton}/>
+			<CountDownTimer />
 		</>
 	);
 }
--- app/src/components/CountDownTimer.js
+++ app/src/components/CountDownTimer.js
@@ -4,7 +4,12 @@ import Clock from "./Clock";
 import TimerButtons from "./TimerButtons";
 import styles from "../css/CountDownTimer.css";
 
-export default function CountDownTimer({ onStartButton = f => f }) {
+export default function CountDownTimer() {
+
+	const onStartButton = (hour, minutes, second) => {
+		alert(`${hour}:${minutes}:${second}`);
+	};
+
     return (
         <div className={styles.outer}>
             <section>
