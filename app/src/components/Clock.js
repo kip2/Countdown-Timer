4a80a57ac2dfedc83df9fd91cac7ee0bc702261a
kip2 <tqa1no4@gmail.com>
2023-10-28 16:19:01 +0900

    Clockの見た目を調整

--- app/src/components/Clock.js
+++ app/src/components/Clock.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../css/Clock.css";
 
 export default function Clock() {
     return (
-        <>
-            <h3>00:00:00</h3>
-        </>
+        <div className={styles.clock}>
+            <h3 >00:00:00</h3>
+        </div>
     );
 }
--- app/src/components/CountDownTimer.js
+++ app/src/components/CountDownTimer.js
@@ -2,14 +2,17 @@ import React from "react";
 import Input from "./Input";
 import Clock from "./Clock";
 import TimerButtons from "./TimerButtons";
+import styles from "../css/CountDownTimer.css";
 
 export default function CountDownTimer() {
     return (
-        <>
-            <h2>CountDownTimer</h2>
-            <Input />
-            <Clock />
-            <TimerButtons />
-        </>
+        <div className={styles.outer}>
+            <section>
+                <h2>CountDownTimer</h2>
+                <Input />
+                <Clock />
+                <TimerButtons />
+            </section>
+        </div>
     );
 }
